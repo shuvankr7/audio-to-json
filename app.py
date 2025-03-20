6f871281b3c638ba6fe4c0083b0acde7534d1557
shuvankr7 <92281019+shuvankr7@users.noreply.github.com>
2025-03-20 07:16:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ DEFAULT_MAX_TOKENS = 1024
 def load_whisper_model():
     try:
         import whisper
-        return whisper.load_model("small")
+        return whisper.load_model("base")
     except ImportError:
         st.error(" Whisper module not found. Please ensure it's installed correctly.")
         st.stop()
